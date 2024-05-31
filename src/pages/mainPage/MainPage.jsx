de54f384dad403eb6c48b4032703d5e7a7164104
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-05-31 23:11:02 +0600

    navbar customized

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -3,7 +3,7 @@ import Navbar from "../../shared/Navbar/Navbar";
 
 const MainPage = () => {
     return (
-        <div>
+        <div className="roboto">
             <Navbar/>
             <Outlet/>
         </div>
--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,16 +1,57 @@
 import { useState } from "react";
 import logo from "../../assets/logo.png";
 import { NavLink } from "react-router-dom";
+import { Button, Menu, MenuItem } from "@mui/material";
+import { SiShopee } from "react-icons/si";
 
 const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const menu = (
+    <>
+      <li>
+        <a
+          href="#"
+          className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 uppercase"
+          aria-current="page"
+        >
+          Home
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 uppercase"
+        >
+          Shop
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          className="flex relative gap-2 py-2 px-3  text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 uppercase"
+        >
+          <SiShopee className="text-xl"/> card
+          <span className="text-sm absolute -top-2 -right-3 price-color">0</span>
+        </a>
+      </li>
+    </>
+  );
   return (
     <>
       <nav className="bg-blue-100 border-gray-200 ">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <NavLink
-            to='/'
+            to="/"
             className="flex items-center space-x-1 rtl:space-x-reverse"
           >
             <img src={logo} className="h-10" alt="Flowbite Logo" />
@@ -18,7 +59,38 @@ const Navbar = () => {
               Medicine
             </span>
           </NavLink>
-          <div className="flexn relative items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <div className="flex relative gap-3 items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <div>
+              <Button
+              className="buttonDefaultStyle"
+                id="demo-positioned-button"
+                aria-controls={open ? "demo-positioned-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+              >
+                Language
+              </Button>
+              <Menu
+                id="demo-positioned-menu"
+                aria-labelledby="demo-positioned-button"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+              >
+                <MenuItem onClick={handleClose}>বাংলা</MenuItem>
+                <MenuItem onClick={handleClose}>English</MenuItem>
+              </Menu>
+            </div>
+            <Button className="buttonStyle" variant="outlined">Join US</Button>
             <button
               type="button"
               className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 "
@@ -35,7 +107,7 @@ const Navbar = () => {
             </button>
             {dropdownOpen && (
               <div
-                className="z-50 absolute top-5 right-0 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow "
+                className="z-50 absolute top-7 right-0 my-4 text-base list-none bg-blue-200 divide-y divide-gray-100 rounded-lg shadow "
                 id="user-dropdown"
               >
                 <div className="px-4 py-3">
@@ -50,7 +122,7 @@ const Navbar = () => {
                   <li>
                     <a
                       href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white"
                     >
                       Dashboard
                     </a>
@@ -58,25 +130,17 @@ const Navbar = () => {
                   <li>
                     <a
                       href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white"
                     >
-                      Settings
+                      Update Profile
                     </a>
                   </li>
                   <li>
                     <a
                       href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white"
                     >
-                      Earnings
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    >
-                      Sign out
+                      Log out
                     </a>
                   </li>
                 </ul>
@@ -115,47 +179,7 @@ const Navbar = () => {
             id="navbar-user"
           >
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-blue-100">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 "
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
-                >
-                  Services
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
-                >
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
-                >
-                  Contact
-                </a>
-              </li>
+              {menu}
             </ul>
           </div>
         </div>
